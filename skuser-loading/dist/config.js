4c095c5c13753c97bc5aef25b6526306010afb17
skusers12 <skuserdev@gmail.com>
2026-01-02 03:22:26 +0100

    Add files via upload

---
+++ skuser-loading/dist/config.js
@@ -0,0 +1,43 @@
+window.LoadingScreenConfig = {
+  media: [
+    { 
+      src: 'https://r2.fivemanage.com/Dhmb4iseJYOtB6BPN80lv/SENTINOFENOMENprod.Manute(OfficialVIDEO).mp4', 
+      title: 'Fenomen',
+      artist: 'Sentino',
+      cover: 'https://t2.genius.com/unsafe/344x344/https%3A%2F%2Fimages.genius.com%2F453acf44c6895ea06be487369cf54dea.1000x1000x1.png'
+    },
+    { 
+      src: 'https://r2.fivemanage.com/Dhmb4iseJYOtB6BPN80lv/SENTINO-DYLEMAT(prod.Manute).mp4', 
+      title: 'Dylemat',
+      artist: 'Sentino',
+      cover: 'https://t2.genius.com/unsafe/344x344/https%3A%2F%2Fimages.genius.com%2F13fe708147b44f61036a7a14f3faf063.1000x1000x1.png'
+    },
+  ],
+
+  team: [
+    {
+      avatar: 'https://i.pravatar.cc/150?img=1',
+      nick: 'Skuser',
+      role: 'Owner',
+      roleColor: '#d60000'
+    },
+  ],
+
+  theme: {
+    primaryColor: '#4400ffff',
+    primaryColorAlpha: '#4400ff40'
+  },
+
+  audio: {
+    defaultVolume: 10,
+    autoPlay: true
+  },
+
+  loadingTexts: {
+    init: 'Inicjalizacja zasobów...',
+    download: 'Pobieranie danych...',
+    loading: 'Ładowanie świata...',
+    almost: 'Prawie gotowe...',
+    done: 'Gotowe! Dołączanie...'
+  }
+}
---
+++ skuser-loading/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Skuser'
+description 'Zajebisty loading'
+
+loadscreen 'dist/index.html'
+
+files {
+    'dist/index.html',
+    'dist/assets/*.js',
+    'dist/assets/*.css',
+    'dist/config.js',
+    'public/media/*.mp4',
+    'public/media/*.jpg',
+    'public/media/*.png'
+}
